3395e00570443cceb48759dcda1700c551f988fa
Krishu <varakrishna73@gmail.com>
2024-11-06 13:41:40 +0530

    Add files via upload

---
+++ Modules/import_local_mex.js
@@ -0,0 +1,12 @@
+const check=require('./local_m_example');
+
+check.oddeven_num(10);
+check.pos_neg_num(-8);
+check.prime_num(12);
+check.fact_num(5);
+check.max_num(12,1);
+check.max_num3(1,23,2);
+check.palind_num(123321);
+check.leap_year(2023);
+check.num_digit(4);
+check.sum_digit(4);
