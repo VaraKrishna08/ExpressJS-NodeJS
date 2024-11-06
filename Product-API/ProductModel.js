6d74d2523df6d760a130dfe132389f7adabac2d3
Krishu <varakrishna73@gmail.com>
2024-11-06 13:32:07 +0530

    Update ProductModel.js

--- Product-API/ProductModel.js
+++ Product-API/ProductModel.js
@@ -1,1 +1,11 @@
+const mongoose=require('mongoose')
+const schema=mongoose.Schema({
+    id:Number,
+    Name:String,
+	Price:Number,
+	Quantity:Number,
+	Category:String,
+	Description:String,
+})
+module.exports=mongoose.model("Product",schema)
 
